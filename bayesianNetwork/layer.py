2565698f906d7c3658f1d0dcf309df134f637fcb
Braden Kronheim <brkronheim@davidson.edu>
2019-03-15 08:39:09 -0400

    cleaned code and added documentation

--- bayesianNetwork/layer.py
+++ bayesianNetwork/layer.py
@@ -1,77 +1,67 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Mar  3 11:57:29 2019
-
-@author: brade
-"""
 import numpy as np
 import tensorflow as tf
 import tensorflow_probability as tfp
 
 tfd = tfp.distributions
 
-
-class layer(object):
-    class perceptron(object):
-        """Stores the weight and biases distributions for a single perceptron"""
-        def __init__(self, wMean, wSD, bMean, bSD, inputSize, dtype):
-            """
-            Arguments:
-                * wMean: mean for weight distribution
-                * wSD: standard deviation for weight distribution
-                * bMean: mean for bias distribution
-                * bSD: standard deviation for bias distribution
-            """
-            self.weights = tfd.MultivariateNormalDiag(
-                    loc=np.ones(inputSize, dtype=dtype)*wMean,
-                    scale_diag=np.ones(inputSize, dtype=dtype)*wSD)
-    
-            self.bias = tfd.MultivariateNormalDiag(
-                    loc=[bMean],
-                    scale_diag=[bSD])
-
-            
-    def __init__(self, inputSize, outputSize, dtype):
-        self.inputSize=inputSize
-        self.outputSize=outputSize
+class DenseLayer(object):
+    """Creates a 1 Dimensional Dense Bayesian Layer."""        
+    def __init__(self, inputDims, outputDims, dtype=np.float32):
+        """
+        Arguments:
+            * inputDims: number of input dimensions
+            * outputDims: number of output dimensions
+            * dtype: data type of input and output values
+        """
+        
+        self.inputDims=inputDims
+        self.outputDims=outputDims
         self.dtype=dtype
         
-        self.allWeights=[None]*outputSize
-
+        self.allWeights=[None]*outputDims #List to store all weight distributions
+        
+        #Weight mean value and mean distribution
         self.weightsMean=tf.Variable(0, dtype=self.dtype)
         self.weightsMeanHyperMean=tfd.MultivariateNormalDiag(loc=[self.weightsMean],
                     scale_diag=[1.0])
         self.weightsMeanHyperSD=tfd.MultivariateNormalDiag(loc=[self.weightsMean],
                     scale_diag=[1.0])
         
+        #weight SD value and SD distribution
         self.weightsSD=tf.Variable(1, dtype=self.dtype)
         self.weightsSDHyperMean=tfd.MultivariateNormalDiag(loc=[self.weightsSD],
                     scale_diag=[1.0])
         self.weightsSDHyperSD=tfd.MultivariateNormalDiag(loc=[self.weightsMean],
                     scale_diag=[1.0])
 
-        self.allBiases=[None]*outputSize
-
+        self.allBiases=[None]*outputDims #List to store all bias distributions
+        
+        #bias mean value and mean distribution
         self.biasesMean=tf.Variable(0, dtype=self.dtype)
         self.biasesMeanHyperMean=tfd.MultivariateNormalDiag(loc=[self.weightsMean],
                     scale_diag=[1.0])
         self.biasesMeanHyperSD=tfd.MultivariateNormalDiag(loc=[self.weightsMean],
                     scale_diag=[1.0])
 
+        #bias SD value and SD distribution
         self.biasesSD=tf.Variable(1, dtype=self.dtype)
         self.biasesSDHyperMean=tfd.MultivariateNormalDiag(loc=[self.weightsSD],
                     scale_diag=[1.0])
         self.biasesSDHyperSD=tfd.MultivariateNormalDiag(loc=[self.weightsMean],
                     scale_diag=[1.0])
+        
+        #Starting weight mean, weight SD, bias mean, and bias SD
         self.firstHypers=np.float32([0.0, 1.0, 0.0, 1.0])
 
-        for n in range(self.outputSize):
-            node=layer.perceptron(self.weightsMean, self.weightsSD, self.biasesMean, self.biasesSD, self.inputSize, self.dtype)
-            self.allWeights[n]=node.weights
-            self.allBiases[n]=node.bias
-        
-        self.weights_chain_start = tf.placeholder(dtype, shape=(self.outputSize, self.inputSize))
-        self.bias_chain_start = tf.placeholder(dtype, shape=(self.outputSize, 1))
+        #Create the weight and bias distributions for the perceptrons in a layer
+        for n in range(self.outputDims):
+            self.allWeights[n], self.allBiases[n] = self.createPerecptron(
+                    self.weightsMean, self.weightsSD, self.biasesMean, 
+                    self.biasesSD, self.inputDims, self.dtype)
+            
+        #Placeholders for the weights, biases, and their means and SDs for use in HMC
+        self.weights_chain_start = tf.placeholder(dtype, shape=(self.outputDims, self.inputDims))
+        self.bias_chain_start = tf.placeholder(dtype, shape=(self.outputDims, 1))
         
         self.weight_mean_chain_start = tf.placeholder(dtype, shape=())
         self.weight_SD_chain_start = tf.placeholder(dtype, shape=())
@@ -79,17 +69,29 @@ class layer(object):
         self.bias_mean_chain_start = tf.placeholder(dtype, shape=())
         self.bias_SD_chain_start = tf.placeholder(dtype, shape=())
         
+    
+    def createPerceptron(self):
+        """Creates the weight and biases distributions for a single perceptron"""
+        weights = tfd.MultivariateNormalDiag(
+                loc=np.ones(self.inputDims, dtype=self.dtype)*self.weightsMean,
+                scale_diag=np.ones(self.inputDims, dtype=self.dtype)*self.weightsSD)
+
+        bias = tfd.MultivariateNormalDiag(
+                loc=[self.biasesMean],
+                scale_diag=[self.biasesSD])
         
+        return(weights, bias)
         
         
     def update(self, weightsMean, weightsSD, biasesMean, biasesSD):
+        """Updates the weight and bias distributions for the layer when their
+        means and SDs change.
+        """
         self.weightsMean=tf.Variable(weightsMean, dtype=self.dtype)
         self.weightsSD=tf.Variable(weightsSD, dtype=self.dtype)
         self.biasesMean=tf.Variable(biasesMean, dtype=self.dtype)
         self.biasesSD=tf.Variable(biasesSD, dtype=self.dtype)
-        for n in range(self.outputSize):
-            node=layer.perceptron(self.weightsMean, self.weightsSD, self.biasesMean, self.biasesSD, self.inputSize, self.dtype)
-            self.allWeights[n]=node.weights
-            self.allBiases[n]=node.bias
-        
-  
+        for n in range(self.outputDims):
+            self.allWeights[n], self.allBiases[n] = self.createPerecptron(
+                    self.weightsMean, self.weightsSD, self.biasesMean, 
+                    self.biasesSD, self.inputDims, self.dtype)  
