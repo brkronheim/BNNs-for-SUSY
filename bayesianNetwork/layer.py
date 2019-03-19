c875d1d6866991c9d811cb3d9128379aa6b99a6a
Braden Kronheim <brkronheim@davidson.edu>
2019-03-19 11:56:05 -0400

    significant cleaning and streamlining

--- bayesianNetwork/layer.py
+++ bayesianNetwork/layer.py
@@ -20,18 +20,20 @@ class DenseLayer(object):
         
         self.allWeights=[None]*outputDims #List to store all weight distributions
         
-        #Weight mean value and mean distribution
         self.weightsMean=tf.Variable(0, dtype=self.dtype)
+            
+        #Weight mean value and mean distribution
+
         self.weightsMeanHyperMean=tfd.MultivariateNormalDiag(loc=[self.weightsMean],
                     scale_diag=[1.0])
-        self.weightsMeanHyperSD=tfd.MultivariateNormalDiag(loc=[self.weightsMean],
+        self.weightsMeanHyperSD=tfd.MultivariateNormalDiag(loc=[1.0],
                     scale_diag=[1.0])
         
         #weight SD value and SD distribution
         self.weightsSD=tf.Variable(1, dtype=self.dtype)
         self.weightsSDHyperMean=tfd.MultivariateNormalDiag(loc=[self.weightsSD],
                     scale_diag=[1.0])
-        self.weightsSDHyperSD=tfd.MultivariateNormalDiag(loc=[self.weightsMean],
+        self.weightsSDHyperSD=tfd.MultivariateNormalDiag(loc=[1.0],
                     scale_diag=[1.0])
 
         self.allBiases=[None]*outputDims #List to store all bias distributions
@@ -40,24 +42,21 @@ class DenseLayer(object):
         self.biasesMean=tf.Variable(0, dtype=self.dtype)
         self.biasesMeanHyperMean=tfd.MultivariateNormalDiag(loc=[self.weightsMean],
                     scale_diag=[1.0])
-        self.biasesMeanHyperSD=tfd.MultivariateNormalDiag(loc=[self.weightsMean],
+        self.biasesMeanHyperSD=tfd.MultivariateNormalDiag(loc=[1.0],
                     scale_diag=[1.0])
 
         #bias SD value and SD distribution
         self.biasesSD=tf.Variable(1, dtype=self.dtype)
         self.biasesSDHyperMean=tfd.MultivariateNormalDiag(loc=[self.weightsSD],
                     scale_diag=[1.0])
-        self.biasesSDHyperSD=tfd.MultivariateNormalDiag(loc=[self.weightsMean],
+        self.biasesSDHyperSD=tfd.MultivariateNormalDiag(loc=[1.0],
                     scale_diag=[1.0])
         
         #Starting weight mean, weight SD, bias mean, and bias SD
         self.firstHypers=np.float32([0.0, 1.0, 0.0, 1.0])
-
         #Create the weight and bias distributions for the perceptrons in a layer
         for n in range(self.outputDims):
-            self.allWeights[n], self.allBiases[n] = self.createPerecptron(
-                    self.weightsMean, self.weightsSD, self.biasesMean, 
-                    self.biasesSD, self.inputDims, self.dtype)
+            self.allWeights[n], self.allBiases[n] = self.createPerceptron()
             
         #Placeholders for the weights, biases, and their means and SDs for use in HMC
         self.weights_chain_start = tf.placeholder(dtype, shape=(self.outputDims, self.inputDims))
@@ -82,6 +81,49 @@ class DenseLayer(object):
         
         return(weights, bias)
         
+    def calculateProbs(self, weights, biases):
+        """Calculates the log probability of a set of weights and biases given
+        their distributions in this layer.
+        """
+        prob=0
+        for m in range(self.outputDims):     
+            val=self.allWeights[m].log_prob([weights[m]])
+            prob+=tf.reduce_sum(val)
+    
+            val=self.allBiases[m].log_prob([biases[m]])
+            prob+=tf.reduce_sum(val)
+        return(prob)
+        
+    def calculateHyperProbs(self, weights, weightsMean, weightsSD, biases, biasesMean, biasesSD):
+        """Calculates the log probability of a set of weights and biases given
+        new distribtuions as well as the probability of the new distribution
+        means and SDs given their distribtuions.
+        """
+        prob=0
+
+        val=self.weightsMeanHyperMean.log_prob([[weightsMean]])
+        prob+=tf.reduce_sum(val)
+        val=self.weightsMeanHyperSD.log_prob([[weightsSD]])
+        prob+=tf.reduce_sum(val)
+
+        val=self.biasesMeanHyperMean.log_prob([[biasesMean]])
+        prob+=tf.reduce_sum(val)
+        val=self.biasesMeanHyperSD.log_prob([[biasesSD]])
+        prob+=tf.reduce_sum(val)
+
+        weightsDist = tfd.MultivariateNormalDiag(
+                loc=np.ones(self.inputDims, dtype=self.dtype)*weightsMean,
+                scale_diag=np.ones(self.inputDims, dtype=self.dtype)*weightsSD)
+
+        biasDist = tfd.MultivariateNormalDiag(
+                loc=[biasesMean],
+                scale_diag=[biasesSD])
+        val=weightsDist.log_prob([weights])
+        prob+=tf.reduce_sum(val)    
+        val=biasDist.log_prob([biases])
+        prob+=tf.reduce_sum(val)
+
+        return(prob)    
         
     def update(self, weightsMean, weightsSD, biasesMean, biasesSD):
         """Updates the weight and bias distributions for the layer when their
@@ -92,6 +134,4 @@ class DenseLayer(object):
         self.biasesMean=tf.Variable(biasesMean, dtype=self.dtype)
         self.biasesSD=tf.Variable(biasesSD, dtype=self.dtype)
         for n in range(self.outputDims):
-            self.allWeights[n], self.allBiases[n] = self.createPerecptron(
-                    self.weightsMean, self.weightsSD, self.biasesMean, 
-                    self.biasesSD, self.inputDims, self.dtype)  
+            self.allWeights[n], self.allBiases[n] = self.createPerceptron()  
