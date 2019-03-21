e74aca8f6bfa5fb5b0a4a1a40a9d6ef884b9d280
Braden Kronheim <brkronheim@davidson.edu>
2019-03-21 11:43:33 -0400

    Cleaned and added more documentation

--- bayesianNetwork/network.py
+++ bayesianNetwork/network.py
@@ -11,32 +11,31 @@ class network(object):
     a Bayesian Neural Network using Hamiltonian Monte Carlo and then training
     the network.
     """
-    def __init__(self, inputDims, hiddenDims, hiddenLayers, outputDims, 
-                 dtype, trainX, trainY, validateX, validateY):
+    def __init__(self, dtype, trainX, trainY, validateX, validateY):
         """
         Arguments:
-            
+            * dtype: data type for Tensors
+            * trainX: the training data input
+            * trainY: the training data output
+            * validateX: the validation data input
+            * validateY: the validation data output
         
         """
-        self.inputDims = inputDims
-        self.hiddenDims = hiddenDims
-        self.hiddenLayers = hiddenLayers
-        self.outputDims = outputDims
         self.dtype = dtype
 
-        self.trainX = tf.reshape(tf.constant(trainX, dtype=self.dtype),[len(trainX),1])
+        self.trainX = tf.reshape(tf.constant(trainX, dtype=self.dtype),[len(trainX),len(trainX[0])])
         self.trainY = tf.constant(trainY, dtype=self.dtype)        
 
-        self.validateX = tf.reshape(tf.constant(validateX, dtype=self.dtype),[len(validateX),1])
+        self.validateX = tf.reshape(tf.constant(validateX, dtype=self.dtype),[len(validateX),len(validateX[0])])
         self.validateY = tf.constant(validateY, dtype=self.dtype)        
 
-        self.vars_=[]
-        self.hyperVars=[]
+        self.vars_=[] #List with the current weight and bias values
+        self.hyperVars=[] #List with the current values of the hyper parameters
 
-        self.states=[]
-        self.hyperStates=[]
+        self.states=[] #List with the weight and bias state placeholders
+        self.hyperStates=[] # List with hyper parameter state placeholders
 
-        self.layers=[]
+        self.layers=[] #List of all the layers
 
                 
         
@@ -58,19 +57,23 @@ class network(object):
             scale=np.array(.1, current.dtype.as_numpy_dtype))
         return(result) 
         
-    def metrics(self, train, mean=1, sd=1, *argv):
+    def metrics(self, scaleExp, train, mean=1, sd=1, *argv):
         """Calculates the average squared error and percent difference of the 
         current network
         Arguments:
+            * scaleExp: boolean value to determine whether to take the exponential
+            of the data
+            * train: boolean value to determine whether to use the training data 
             * mean: mean value used for unshifiting a distribution
             * sd: sd value used for unscalling a distribution
             * argv: an undetermined number of tensors containg the weights
-            and biases
+            and biases. If none are given the stored values are used
         Returns:
             * squaredError: the mean squared error of predictions from the network
             * percentError: the percent error of the predictions from the network
         
         """
+        #Determine whether weight and bias tensors were given
         tensors=argv
         if(len(tensors)==0):
             tensors=self.vars_
@@ -86,6 +89,9 @@ class network(object):
         squaredError=tf.reduce_mean(tf.squared_difference(current, y))
         current=tf.add(tf.multiply(current, sd), mean)
         real=tf.add(tf.multiply(y, sd), mean)
+        if(scaleExp):
+            current=tf.exp(current)
+            real=tf.exp(real)
         percentError=tf.reduce_mean(tf.multiply(tf.abs(tf.divide(tf.subtract(current, real), real)), 100))
         return(squaredError, percentError)
         
@@ -94,6 +100,9 @@ class network(object):
         """Calculates the log probability of the current weight and bias values
         as well as the log probability of their prediction.
         
+        Arguments:
+            * argv: an undetermined number of tensors containg the weights
+            and biases.
         """
 
         prob=tf.reduce_sum(self.make_response_likelihood(argv).log_prob(self.trainY))
@@ -103,6 +112,12 @@ class network(object):
         return(prob)
 
     def calculateHyperProbs(self, *argv):
+        """Calculates the log probability of the current hyper parameters
+        
+        Arguments:
+            * argv: an undetermined number of tensors containg the hyper parameters
+        """
+        
         prob=0
         for n in range(len(self.layers)):
             prob+=self.layers[n].calculateHyperProbs(self.vars_[2*n],
@@ -112,6 +127,13 @@ class network(object):
 
     
     def predict(self, train, *argv):
+        """Makes a prediction
+        
+        Arguments:
+            * train: a boolean value which determines whether to use training data
+            * argv: an undetermined number of tensors containg the weights
+            and biases.
+        """
         tensors=argv
         if(len(tensors)==0):
             tensors=self.vars_
@@ -138,43 +160,25 @@ class network(object):
         current=tf.add(tf.matmul(expandedStates[-2], current), expandedStates[-1])
         return(current)
     
-    def createInitialChains(self):
-
-        self.hyperVars=[]
-        tempWeights0 = tf.random_normal([self.hiddenDims, self.inputDims], dtype =self.dtype)
-        tempBiases0 = tf.random_normal([self.hiddenDims, 1], dtype  =self.dtype)
-        
-        self.vars_.append(tempWeights0)
-        self.vars_.append(tempBiases0)
-        
-        for n in range(self.hiddenLayers-1):
-            tempWeights0 = tf.random_normal([self.hiddenDims, self.hiddenDims], dtype=self.dtype)
-            tempBiases0 = tf.random_normal([self.hiddenDims, 1], dtype=self.dtype)
-            
-            self.vars_.append(tempWeights0)
-            self.vars_.append(tempBiases0)
-
-        tempWeights0 = tf.random_normal([self.outputDims, self.hiddenDims], dtype=self.dtype)
-        tempBiases0 = tf.random_normal([self.outputDims, 1], dtype =self.dtype)
-        
-        self.vars_.append(tempWeights0)
-        self.vars_.append(tempBiases0)
-
-        for layers in self.layers:
-            for val in layers.firstHypers:    
-                self.hyperVars.append(val)
-   
-    
     def update(self):
+        """Updates the hyper parameters in each of the layers"""
         for n in range(len(self.layers)):
             self.layers[n].update(self.hyperVars[4*n],self.hyperVars[4*n+1],
                        self.hyperVars[4*n+2],self.hyperVars[4*n+3])
         
     def add(self, layer):
+        """Adds a new layer to the network
+        Arguments:
+            * layer: the layer to be added
+        """
         self.layers.append(layer)
         
         self.states.append(layer.weights_chain_start)
         self.states.append(layer.bias_chain_start)
+        weights, biases = layer.sampleWeightsBiases()
+        
+        self.vars_.append(weights)
+        self.vars_.append(biases)
         
         self.hyperStates.append(layer.weight_mean_chain_start)
         self.hyperStates.append(layer.weight_SD_chain_start)
@@ -182,24 +186,22 @@ class network(object):
         self.hyperStates.append(layer.bias_mean_chain_start)
         self.hyperStates.append(layer.bias_SD_chain_start)
         
+        self.hyperVars.append(layer.weightsMean)
+        self.hyperVars.append(layer.weightsSD)
+        self.hyperVars.append(layer.biasesMean)
+        self.hyperVars.append(layer.biasesSD)
+        
+    def setupMCMC(self, stepSize, hyperStepSize, leapfrog, hyperLeapfrog):
+        """Sets up the MCMC algorithms
+        Arguments:
+            * stepSize: the starting step size for the weights and biases
+            * hyperStepSize: the starting step size for the hyper parameters
+            * leapfrog: number of leapfrog steps for weights and biases
+            * hyperLeapfrog: leapfrog steps for hyper parameters
         """
-        tempWeights = tf.random_normal([layer.inputDims, layer.outputDims], dtype =self.dtype)
-        tempBiases= tf.random_normal([self.outputDims, 1], dtype  =self.dtype)
         
-        self.vars_.append(tempWeights)
-        self.vars_.append(tempBiases)
-        
-        for val in layer.firstHypers:    
-            self.hyperVars.append(val)
-        """
-        
-    def setup(self):
-        self.createInitialChains()
-        self.setupMCMC()
-        
-    def setupMCMC(self):
-        self.step_size = tf.Variable(np.array(0.0001, self.dtype))
-        self.hyper_step_size = tf.Variable(np.array(0.001, self.dtype))
+        self.step_size = tf.Variable(np.array(stepSize, self.dtype))
+        self.hyper_step_size = tf.Variable(np.array(hyperStepSize, self.dtype))
         num_results = 2 #number of markov chain draws
         
         self.states_MCMC, kernel_results = tfp.mcmc.sample_chain(
@@ -208,7 +210,7 @@ class network(object):
             current_state=self.states, #starting parts of chain 
             kernel=tfp.mcmc.HamiltonianMonteCarlo( #use HamiltonianMonteCarlo to step in the chain
                 target_log_prob_fn=self.calculateProbs, #used to calculate log density
-                num_leapfrog_steps=20,
+                num_leapfrog_steps=leapfrog,
                 step_size=self.step_size,
                 
                 step_size_update_fn=tfp.mcmc.make_simple_step_size_update_policy(decrement_multiplier=0.01), 
@@ -224,7 +226,7 @@ class network(object):
             current_state=self.hyperStates, #starting parts of chain 
             kernel=tfp.mcmc.HamiltonianMonteCarlo( #use HamiltonianMonteCarlo to step in the chain
                 target_log_prob_fn=self.calculateHyperProbs, #used to calculate log density
-                num_leapfrog_steps=20,
+                num_leapfrog_steps=hyperLeapfrog,
                 step_size=self.hyper_step_size,
                 
                 step_size_update_fn=tfp.mcmc.make_simple_step_size_update_policy(decrement_multiplier=0.01), 
@@ -233,7 +235,21 @@ class network(object):
             tf.exp(tf.minimum(hyper_kernel_results.log_accept_ratio, 0.)))
         self.hyper_loss = -tf.reduce_mean(hyper_kernel_results.accepted_results.target_log_prob)
         
-    def train(self, epochs, updatePriorsWait, updatePriorsEpochs, startSampling, samplingStep):
+    def train(self, epochs, updatePriorsWait, updatePriorsEpochs, startSampling, samplingStep, mean, sd):
+        """Trains the network
+        Arguements:
+            * Epochs: Number of training cycles
+            * updatePriorsWait: Number of epochs between priror updates
+            * updatePriorsEpochs: Number of training cycles for priors
+            * startSampling: Number of epochs before networks start being saved
+            * samplingStep: Epochs between sampled networks
+            * mean: true mean of output distribution
+            * sd: true sd of output distribution
+            
+        Returns:
+            * results: the output of the network when sampled
+        """
+       
         self.results=[]
         with tf.Session() as sess:
         
@@ -242,7 +258,7 @@ class network(object):
         
             iter_=0
             
-            while(iter_<epochs):
+            while(iter_<epochs): #Main training loop
                 for n in range(len(self.vars_)):
                     if(tf.contrib.framework.is_tensor(self.vars_[n])):
                         self.vars_[n]=sess.run(self.vars_[n])
@@ -262,7 +278,7 @@ class network(object):
                 iter_+=1
                 print('iter:{:>2}  Network loss:{: 9.3f}  step_size:{:.7f}  avg_acceptance_ratio:{:.4f}'.format(
                           iter_, loss_, step_size_, avg_acceptance_ratio_))
-                if(iter_%updatePriorsWait==0):
+                if(iter_%updatePriorsWait==0): #Hyper update loop
                     hyperIter=0
                     while(hyperIter<updatePriorsEpochs):
                         for n in range(len(self.hyperVars)):
@@ -287,8 +303,8 @@ class network(object):
                         if(hyperIter==updatePriorsWait):
                             self.update()
                 
-                if(iter_>=startSampling and iter_%samplingStep==0):
-                    squaredError, percentError=sess.run(self.metrics(False))
+                if(iter_>=startSampling and iter_%samplingStep==0): #Create predictions
+                    squaredError, percentError=sess.run(self.metrics(True, False, mean, sd))
                     print('squaredError{: 9.5f} percentDifference{: 7.3f}'.format(squaredError, percentError))
                     out=self.predict(False)
                     self.results.append(sess.run(out))
