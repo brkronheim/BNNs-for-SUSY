79c2d0da9e299ad8c924244b891a6fdb85d2b2ee
Braden Kronheim <brkronheim@davidson.edu>
2019-05-07 09:50:34 -0400

    cleaned and added network loading

--- bayesianNetwork/BNN_functions.py
+++ bayesianNetwork/BNN_functions.py
@@ -1,6 +1,5 @@
 import numpy as np
 import tensorflow as tf
-import tensorflow_probability as tfp
 import math
 
 from scipy import stats
@@ -66,6 +65,7 @@ def normalizeData(trainIn, trainOut, valIn, valOut):
 def multivariateLogProb(sigma, mu, x):
     """ Calculates the log probability of x given mu and sigma defining 
     a multivariate normal distribution. 
+    
     Arguments:
         * sigma: an n-dimensional vector with the standard deviations of
         * the distribution
@@ -73,8 +73,8 @@ def multivariateLogProb(sigma, mu, x):
         * x: m n-dimensional vectors to have their probabilities calculated
     Returns:
         * prob: an m-dimensional vector with the log-probabilities of x
-    
     """
+    
     sigma*=sigma
     sigma=tf.maximum(sigma, np.float32(10**(-16)))
     sigma=tf.minimum(sigma, np.float32(10**(16)))
@@ -87,5 +87,64 @@ def multivariateLogProb(sigma, mu, x):
     logLikelihood=-0.5*(logDet+tf.matmul(
             tf.matmul(tf.transpose(dif),inv),
             (dif))+k*tf.log(2*math.pi))   
-    prob=tf.linalg.diag_part(logLikelihood)
-    return(prob)     
+    return(tf.linalg.diag_part(logLikelihood))
+
+def loadNetworks(directoryPath):
+    """Loads saved networks.
+    
+    Arguments:
+        * directoryPath: the path to the directory where the networks are saved
+    Returns:
+        * numNetworks: Total number of networks 
+        * numMatrices: Number of matrices in the network
+        * matrices: A list containing all the extracted matrices
+    
+    """
+    summary=[]
+    with open(directoryPath+"summary.txt","r") as file:
+        for line in iter(file):
+            summary.append(line.split())
+    
+    numNetworks=int(summary[-1][0])
+    numMatrices=int(summary[-1][2])
+    numFiles=int(summary[-1][1])
+
+    matrices=[]
+    for n in range(numMatrices):
+        weightsSplitDims=(numNetworks*numFiles,int(summary[n][0]),int(summary[n][1]))
+        weights0=np.zeros(weightsSplitDims)
+        for m in range(numFiles):
+            weights=np.loadtxt(directoryPath+str(n)+"."+str(m)+".txt", dtype=np.float32,ndmin=2)
+            print(weights.shape)
+            print(weightsSplitDims)
+            for k in range(numNetworks):
+                weights0[m*numNetworks+k,:,:]=weights[weightsSplitDims[1]*k:weightsSplitDims[1]*(k+1),:weightsSplitDims[2]]
+        matrices.append(weights0)
+    numNetworks*=numFiles
+    return(numNetworks, numMatrices, matrices)
+    
+def predict(inputMatrix, numNetworks, numMatrices, matrices):
+    """Make predictions from an ensemble of neural networks. 
+    
+    Arguments:
+        * inputMatrix: The input data
+    Returns:
+        * numNetworks: Number of networks used
+        * numMatrices: Number of matrices in the network
+        * matrices: List with all networks used
+    """
+    
+    inputVal=np.transpose(inputMatrix)
+    initialResults=[None]*(numNetworks)
+    for m in range(numNetworks):
+        current=inputVal
+        for n in range(0,numMatrices,2):
+            current=np.matmul(matrices[n][m,:,:],current)
+            current+=matrices[n+1][m,:,:]
+            if(n+2<numMatrices):
+                current=np.maximum(current,0)
+        if(m%100==0):
+            print(m/numNetworks/numFiles)
+        initialResults[m]=current
+        
+    return(initialResults)
